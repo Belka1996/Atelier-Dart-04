ded342c14b856b4d6d8c2e40ef74d3133dad093f
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-12 21:40:58 -0500

    Atelier4 Exo 7 étape 5 ok

--- Atelier 4 exo7.dart
+++ Atelier 4 exo7.dart
@@ -5,3 +5,11 @@ class Tache {
    Tache(this.description){
     nombreTotal++;
   }
+}
+void main() {
+  var t1 = Tache("Préparer le rapport");
+  var t2 = Tache("Envoyer les emails");
+  var t3 = Tache("Sauvegarder les fichiers");
+
+  print("Nombre total de tâches : ${Tache.nombreTotal}");
+}
