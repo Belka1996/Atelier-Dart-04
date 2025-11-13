a34aa0ad5c40208b20e50158f16555269a9664b5
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-12 20:34:40 -0500

    Atelier4 Exo 5 étape 5 ok

--- Atelier 4 exo5.dart
+++ Atelier 4 exo5.dart
@@ -2,6 +2,14 @@
 abstract class Forme {
   double calculerAire();
   void afficherMessage() {
-    print("Calcul de l'aire");
+    print("Calcul de l'aire...");
+  }
+}
+class Cercle extends Forme {
+  final double rayon;
+  Cercle(this.rayon);
+  @override
+  double calculerAire(){
+    return 3.14*rayon*rayon;
   }
 }
