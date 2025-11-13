69511f4511c380ff193a555cad56350e6ef31f91
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-12 19:44:53 -0500

    Atelier4 Exo 4 étape 6 ok

--- Atelier 4 exo4.dart
+++ Atelier 4 exo4.dart
@@ -6,3 +6,11 @@ class Media {
     print("ceci est un média générique.");
   }
 }
+class livre extends Media {
+  final String auteur;
+  livre(String titre, this.auteur) : super(titre);
+  @override
+  void afficherType(){
+    print("Ceci est un livre : '$titre' par $auteur.");
+  }
+}
