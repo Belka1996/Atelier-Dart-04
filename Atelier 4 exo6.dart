b3167bc15c16a0520a8d03132968d265e0ad121c
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-12 20:58:32 -0500

    Atelier4 Exo 6 étape 5 ok

--- Atelier 4 exo6.dart
+++ Atelier 4 exo6.dart
@@ -3,3 +3,13 @@ class Connectable {
   void connecter(String utilisateur);
   void desconnecter();
 }
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur !");
+  }
+  @override
+  void desconnecter(){
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
