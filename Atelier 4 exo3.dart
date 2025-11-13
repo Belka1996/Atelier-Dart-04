40a3db78393d8a7a10ac3e4626896fc6087d755d
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-12 19:12:41 -0500

    Atelier4 Exo 3 étape 5 ok

--- Atelier 4 exo3.dart
+++ Atelier 4 exo3.dart
@@ -4,13 +4,8 @@ class Compte {
  final String numeroCompte;
   double _solde = 0.0;
   Compte(this.numeroCompte, [this._solde = 0.0]);
-  String get solde => "${_solde.toStringAsFixed(2)} \$";
-  void depot(double montant) {
-    if (montant > 0) {
-      _solde +=montant;
-      print("Dépot de $montant \$ effectué !");
-    } else {
-      print("Montant invalide, dépot refusé.");
-    }
+  String get solde{
+    return "${_solde.toStringAsFixed(2)} \$";
   }
+
 }
